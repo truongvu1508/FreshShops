aa3d75dcd460a87f95d324f6074ad49ecaa14c19
dothanhbao1805 <dothanhbao42@gmail.com>
2024-12-07 16:06:29 +0700

    bao_update

---
+++ FreshShop/Models/OrderDetails.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreshShop.Models
+{
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string OrderCode { get; set; }
+
+        public int ProductId { get; set; }  // Khóa ngoại tham chiếu tới bảng Product
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        // Mối quan hệ giữa OrderDetails và ProductModel
+        [ForeignKey("ProductId")]
+        public ProductModel Product { get; set; }
+
+        public int OrderId { get; set; } // Khóa ngoại trỏ tới Order
+        public OrderModel Order { get; set; } // Mối quan hệ với Order
+
+        // Tính thành tiền cho đơn hàng
+        [NotMapped]
+        public decimal TotalPrice => Quantity * Price;
+    }
+}
---
+++ FreshShop/Models/OrderModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FreshShop.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string OrderCode { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Status { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+    }
+}
