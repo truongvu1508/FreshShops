4d3d2a656eefd57bdb5f0005c6991bd9abf7bd57
vicendy04 <htvd1204@gmail.com>
2024-12-08 17:07:00 +0700

    coupon cua admin: list va them o video 95 96

---
+++ FreshShop/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,57 @@
+﻿using FreshShop.Models;
+using FreshShop.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreshShop.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class CouponController : Controller
+	{
+		private readonly DataContext _dataContext;
+		public CouponController(DataContext context)
+		{
+			_dataContext = context;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var coupon_list = await _dataContext.Coupons.ToListAsync();
+			ViewBag.CouponList = coupon_list;
+			return View();
+		}
+		[Route("Create")]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(CouponModel coupon)
+		{
+
+			if (ModelState.IsValid)
+			{
+
+				// Thêm sản phẩm vào cơ sở dữ liệu
+				_dataContext.Add(coupon);
+				await _dataContext.SaveChangesAsync();
+
+				// Thông báo thành công
+				TempData["success"] = "Tạo phiếu giảm giá thành công.";
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				// Thông báo lỗi nếu model không hợp lệ
+				TempData["error"] = "Có lỗi trong quá trình nhập liệu.";
+				List<string> errors = new List<string>();
+				foreach (var value in ModelState.Values)
+				{
+					foreach (var error in value.Errors)
+					{
+						errors.Add(error.ErrorMessage);
+					}
+				}
+				string errormess = string.Join("\n", errors);
+				return BadRequest(errormess);
+			}
+			return View();
+		}
+	}
+}
