19ccbdd9c9f5a1cb91d3d8bb850648fcdd259377
MinhTai139 <phanminhtai139@gmail.com>
2024-12-09 10:12:59 +0700

    mt

---
+++ FreshShop/Models/SliderModel.cs
@@ -0,0 +1,22 @@
+﻿using FreshShop.Repository.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreshShop.Models
+{
+    public class SliderModel
+    {
+        public int Id {  get; set; }
+        [Required(ErrorMessage = "yêu cầu không được bỏ trống tên")]
+        public string Name { get; set; }
+
+        public string Description {  get; set; }
+        public int ? Status { get; set; }
+
+        public string Image {  get; set; }
+
+        [NotMapped]
+        [FileExtension]
+        public IFormFile? ImageUpload { get; set; }
+    }
+}
--- FreshShop/Repository/DataContext.cs
+++ FreshShop/Repository/DataContext.cs
@@ -17,9 +17,12 @@ namespace FreshShop.Repository
 		public DbSet<CouponModel> Coupons { get; set; }
 		public DbSet<ShippingModel> Shipping { get; set; }
 		public DbSet<Statistical> Statistical { get; set; }
+        public DbSet<ProductQuantityModel> ProductQuantities { get; set; }
+        public DbSet<SliderModel> Sliders { get; set; }
+        public DbSet<CompareModel> Compares { get; set; }
+        public DbSet<WishlistModel> Wishlists { get; set; }
 
-
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
